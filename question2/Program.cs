56ec3dba806dde59d68aa57d7ab738ac159dfe41
Onurege <78586675+onure9e@users.noreply.github.com>
2022-12-04 15:23:14 +0300

    Add files via upload

---
+++ question2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            ArrayList numberList = new ArrayList(); 
+            for (int i = 0; i < 20; i++)
+            {
+                number = int.Parse(Console.ReadLine());
+                numberList.Add(number);
+            }
+            numberList.Sort();
+            ArrayList lowNumbers = new ArrayList();
+            ArrayList highNumbers = new ArrayList();
+            for (int i = 0; i < 3; i++)
+            {
+                lowNumbers.Add(numberList[i]);
+            }
+            for (int i = 17; i < 20; i++)
+            {
+                highNumbers.Add(numberList[i]);
+            }
+
+            int totalhigh = 0;
+            Console.Write("en 3 büyük sayı: ");
+            foreach (var item in highNumbers)
+            {
+                Console.Write(item + " ");
+                totalhigh += Convert.ToInt32(item);
+            }
+            int highavarage = totalhigh/3;
+            
+            Console.WriteLine();
+
+            int totallow = 0;
+            Console.Write("En 3 küçük sayı: ");
+            foreach (var item in lowNumbers)
+            {
+                Console.Write(item+" ");
+                totallow += Convert.ToInt32(item);
+            }
+            int lowavarage = totallow/3;
+
+            Console.WriteLine();
+
+            Console.WriteLine("En büyük 3 sayının toplamı : "+totalhigh);
+            Console.WriteLine("En küçük 3 asyının toplamı : " + totallow);
+            Console.WriteLine("ortalama en büyük sayı : " + highavarage);
+            Console.WriteLine("ortalama en küçük sayı :"+lowavarage);
+
+        }
+    }
+}
---
+++ question3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solution of third question
+            Console.WriteLine("> cümle girin: ");
+            string cümle = Console.ReadLine();
+            cümle.ToLower();
+
+            string harfler = "ıiuüoöae";
+
+            int cümleUzunluğu = cümle.Count();
+
+            List<char> harfListesi = new List<char>();
+            for (int i = 0; i < cümleUzunluğu; i++)
+            {
+                if (harfler.Contains(cümle[i]))
+                {
+                    if (!harfListesi.Contains(cümle[i]))
+                    {
+                        harfListesi.Add(cümle[i]);
+                    }
+                 
+                }
+            }
+
+            Console.Write("Bu cümledeki sesli harfler:  ");
+            foreach (var n in harfListesi)
+            {
+                Console.Write(n+", ");
+            }
+
+
+        }
+    }
+}
